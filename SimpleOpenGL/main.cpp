3eb5e54381ea43e3af86d867677b323caa2d3b99
Alex Davidson Bryan <alex.davidsonbryan@gmail.com>
2018-01-16 21:55:39 +0000

    Initial Commit

---
+++ SimpleOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SimpleOpenGL
+//
+//  Created by Alexander Davidson Bryan on 16/01/2018.
+//  Copyright © 2018 Alexander Davidson Bryan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
